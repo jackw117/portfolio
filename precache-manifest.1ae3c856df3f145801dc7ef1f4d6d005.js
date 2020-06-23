9016f779aad9f0629b45760dc08a88468714dc19
Jack Whitesell <me@jackwhitesell.com>
2020-06-22 21:51:29 -0700

    Updates

---
+++ precache-manifest.1ae3c856df3f145801dc7ef1f4d6d005.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9b65c0f16788f0d40921332ea107cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a842a7a735676638a3b5",
+    "url": "/static/css/main.a6bf857c.chunk.css"
+  },
+  {
+    "revision": "8d2ff9a3b9c8e1598dbe",
+    "url": "/static/js/2.3557b7d2.chunk.js"
+  },
+  {
+    "revision": "b89ec519ad42042b552a3d9ce5acde1c",
+    "url": "/static/js/2.3557b7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a842a7a735676638a3b5",
+    "url": "/static/js/main.7aac94dd.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "59f69120e08e96ba195462cf22fbc101",
+    "url": "/static/media/bg.59f69120.jpg"
+  }
+]);
